80718f9cf4ed79ff758807d0106f491c887f6320
pokibon3 <pokibon@gmai.com>
2021-11-14 19:33:12 +0900

    3rd commit

---
+++ AKBONE.bat
@@ -0,0 +1,5 @@
+del .\AKBONE_picovaders\
+claat export -ga "" -h 1f7ert6wRPhKcis4WBwpPZPouY8VuJhr1Iug_wdFAPMs
+git add .
+git commit -m "3rd commit"
+git 1f7ert6wRPhKcis4WBwpPZPouY8VuJhr1Iug_wdFAPMs
